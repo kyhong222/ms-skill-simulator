257258dd381f5d884f625e9362a651dc60e88ca8
khpark <khpark@cognet9.com>
2025-12-19 22:12:11 +0900

    chore: 추가기능 설명 추가

--- src/components/JobSelector/JobSelector.tsx
+++ src/components/JobSelector/JobSelector.tsx
@@ -40,7 +40,7 @@ export default function JobSelector({ onSelect }: { onSelect?: (job: IJob) => vo
         <h1 className="text-2xl font-bold mb-4">9/19 스나이핑 쿨타임 수정</h1>
         <h1 className="text-2xl font-bold mb-4">10/17 돌진 사거리 오류, 마그넷 설명 수정</h1>
         <h1 className="text-2xl font-bold mb-4">10/21 숙련도 오류 수정</h1>
-        <h1 className="text-2xl font-bold mb-4">12/19 패치 반영</h1>
+        <h1 className="text-2xl font-bold mb-4">12/19 패치 반영, 클립보드 복사기능, 스킬트리 자체 저장기능 추가</h1>
       </div>
     </div>
   );
